61575d954845d93f13b48d9d7e9aac8b47dc94d1
hiulit <hiulit@gmail.com>
2021-11-22 10:17:10 +0100

    Added support for showing icons

--- src/core/generateMarkdownFile.js
+++ src/core/generateMarkdownFile.js
@@ -40,6 +40,26 @@ module.exports = file => {
     outputString += `**Extends**: \`${file.extends}\`\n\n`
   }
 
+  if (file.icon) {
+    file.icon = file.icon.replace('res://', '')
+
+    let iconPath = path.dirname(file.icon).split(path.sep)
+    let iconBaseName = path.basename(file.icon)
+    let iconPathArray = []
+
+    for (let index = 0; index < iconPath.length; index++) {
+      const element = iconPath[index]
+      if (!config.projectDir.split(path.sep).includes(element)) {
+        iconPathArray.push(element)
+      }
+    }
+    console.log(path.join(iconPathArray.join(path.sep), iconBaseName))
+    outputString += `**Icon**: ![icon](/${path.join(
+      iconPathArray.join(path.sep),
+      iconBaseName
+    )})\n\n`
+  }
+
   if (!Object.values(file.sections).every(section => section.length === 0)) {
     outputString += `## Table of contents\n\n`
 
